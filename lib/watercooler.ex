af92dba009cd032684fd1c228f2928913fd30faf
CrowdHailer <peterhsaxton@gmail.com>
2018-05-28 13:17:56 +0100

    Add the chatroom

--- lib/watercooler.ex
+++ lib/watercooler.ex
@@ -1,2 +1,18 @@
 defmodule Watercooler do
+  @group :watercooler
+
+  def publish(message) do
+    :ok = :pg2.create(@group)
+    for client <- :pg2.get_members(@group) do
+      send(client, {@group, message})
+    end
+
+    {:ok, message}
+  end
+
+  def listen() do
+    :ok = :pg2.create(@group)
+    :ok = :pg2.join(@group, self())
+    {:ok, @group}
+  end
 end
